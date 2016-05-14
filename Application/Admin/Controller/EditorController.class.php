94419d8c1f596dc30909c9b706c34c4465315384
vslinks <vslinks@qq.com>
2016-05-14 17:35:30 +0800

    添加富文本编辑器图片上传到服务器

---
+++ Application/Admin/Controller/EditorController.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 富文本编辑器服务.
+ * kunx-edu <kunx-edu@qq.com>
+ */
+
+namespace Admin\Controller;
+use Org\Util\Ueditor;
+
+/**
+ * Description of EditorController
+ *
+ * @author qingf
+ */
+class EditorController extends \Think\Controller{
+    
+    /**
+     * ueditor后台服务部分.
+     * 比如获取配置
+     * 比如上传文件和图片,以及读取文件列表.
+     */
+    public function ueditor(){
+        $data = new Ueditor(C('UPLOAD_SETTING'));
+        echo $data->output();
+        exit;
+    }
+}
